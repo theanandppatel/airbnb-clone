89eb18d39451ee20cf849bcf64e62ffa6a83c835
ANANDKUMAR PATEL <anandpatel95374@gmail.com>
2022-09-05 22:40:22 +0530

    Updates

---
+++ precache-manifest.3b154a2c483bd2d4b3aa8fc650297796.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98094d7d65508d0db8c7741579f727b6",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "f686415ad1377a742484",
+    "url": "/airbnb-clone/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "622277bb145c66823f52",
+    "url": "/airbnb-clone/static/css/main.6777df22.chunk.css"
+  },
+  {
+    "revision": "f686415ad1377a742484",
+    "url": "/airbnb-clone/static/js/2.affd6c74.chunk.js"
+  },
+  {
+    "revision": "c79bddbba8fbca3c8f248c31561dc255",
+    "url": "/airbnb-clone/static/js/2.affd6c74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622277bb145c66823f52",
+    "url": "/airbnb-clone/static/js/main.8f8256d7.chunk.js"
+  },
+  {
+    "revision": "f358a9c4d5787a3a04f2",
+    "url": "/airbnb-clone/static/js/runtime-main.fc586be8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/airbnb-clone/precache-manifest.3b154a2c483bd2d4b3aa8fc650297796.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/airbnb-clone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
